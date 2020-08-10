aca3cc992dd1ced7e3b404d543bd0896d813e97d
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-10 04:39:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,6 +2,22 @@
 def game_hash
   {
    home:{
+     team_name: ,
+     colors:{
+     } ,
+     players:{
+       player_name: ,
+       number: ,
+       shoe: ,
+       points: ,
+       rebounds: ,
+       assists: ,
+       steals: ,
+       blocks: ,
+       slam_dunks: 
+     }
+     
+     }
      
    },
    away:{
