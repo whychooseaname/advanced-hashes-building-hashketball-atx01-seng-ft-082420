962b620ef9cfb6550a9d7cb9e56ae6db01ea1cd7
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-10 04:44:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,18 +3,18 @@ def game_hash
   {
    home:{
      team_name: "Brooklyn Nets",
-     colors:{
+     colors:{Black, White
      } ,
      players:{
-       player_name: ,
-       number: ,
-       shoe: ,
-       points: ,
-       rebounds: ,
-       assists: ,
-       steals: ,
-       blocks: ,
-       slam_dunks: 
+       player_name: "Alan Anderson",
+       number: 0,
+       shoe: 16,
+       points: 22,
+       rebounds: 12,
+       assists: 12,
+       steals: 3,
+       blocks: 1,
+       slam_dunks: 1 
      }
     }
    },
